82b156db12fd6a65c919b41ee94fb76fa039af4e
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-03-14 17:26:27 -0400

    Added World and Screen UI

---
+++ Assets/Scripts/UI/MainMenuHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// NEW USING STATEMENTS BELOW
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour {
+
+    public void Play()
+    {
+        // Debug.Log("Play");
+        SceneManager.LoadScene("Week6_2");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
